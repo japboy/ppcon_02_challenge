a2fb186aa30d75f383a90469d18904c61eb774a2
Yu I <lagfer@youck.org>
2012-06-25 17:25:31 +0900

    Answer all of the questions with minimal implementations

--- js/user-edit.js
+++ js/user-edit.js
@@ -5,14 +5,32 @@ $(function() {
 
   });
 
+  var $list = function(content){
+    return '<li>' + content + '</li>';
+  };
+
   $("#add_sakurako").click(function(){
-    $('#member_list').prepend('<li>櫻子</li>');
+    $('#member_list').prepend($list("櫻子"));
   });
 
   $("#add_himawari").click(function(){
-    $('#member_list').prepend('<li>向日葵</li>');
+    $('#member_list').prepend($list("向日葵"));
   });
 
+  $("#remove_akari").click(function(){
+    $("#member_list_remove > #akari").remove();
+  });
 
+  $("#change_visible_momo").toggle(function(){
+    $("#stealth_momo").css({ display: "block" });
+  }, function(){
+    $("#stealth_momo").css({ display: "none" });
+  });
 
-});;
+  $("#change_header_html").click(function(){
+    $("h2").each(function(index, element){
+      $(element).text($(element).text().replace(/(.*)/, "【$1】"));
+    });
+  });
+
+});
